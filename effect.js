70363c5927f1236f2b8b9c01fb5adabb846e4edf
NguyenNhatTungg <qswavippro@gmail.com>
2025-03-17 03:04:18 +0700

    update code

--- effect.js
+++ effect.js
@@ -173,9 +173,9 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(1300).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(2300).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1500);
+			$("p:nth-child("+i+")").fadeIn('slow').delay(2500);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
